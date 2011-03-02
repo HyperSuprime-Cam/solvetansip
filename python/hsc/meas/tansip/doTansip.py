5b8d6f7b042a5e857f4ab04c29444053bc543f0e
Craig Loomis <cloomis@astro.princeton.edu>
2011-03-02 16:53:53 +0900

    content-free cleanup

--- python/hsc/meas/tansip/doTansip.py
+++ python/hsc/meas/tansip/doTansip.py
@@ -1,3 +1,4 @@
+
 import sys, re, os, math
 import signal
 import datetime
@@ -6,6 +7,7 @@ import time
 import lsst.afw.cameraGeom.utils         as cameraGeomUtils
 import hsc.meas.tansip.WCS_PL_MAIN       as hscTansip
 import lsst.daf.base                     as dafBase
+import lsst.pex.policy as pexPolicy
 
 import lsst.afw.display.ds9              as ds9
 
@@ -18,35 +20,33 @@ def doTansip(matchListAllCcd, policy=None, camera=None, rerun=None):
     """ Entry point for calling the global solvetansip fitter.
 
     Args:
-        matchListAllCcd   - SourceMatchList, e.g. from reduceOneCcd.doOneCcd()
-        ppPolicyFile      ? 
+        matchListAllCcd - SourceMatchList, e.g. from runHsc.doRun()
+        policy          - pexPolicy; reads our own policy if None
+        camera          - cameraGeom
+        rerun           - ignored
     """
     
-    # load the policy contents from the ppPolicy file
-    #if not ppPolicyFile:
-    #    ppPolicyFile = pipeUtils.getPolicyFile("protoPipeDC1.paf")
-    #ppPolicy = pexPolicy.Policy.createPolicy(ppPolicyFile)
-
-    #APROPPolicy = pipeUtils.getPolicyFromPolicy(ppPolicy, "tansipPolicyFile")
-    APROPPolicy = policy
+    if not policy:
+        policyPath = os.path.join(os.getenv("SOLVETANSIP_DIR"), "policy", "WCS_MAKEAPROP.paf")
+        policy = pexPolicy.Policy.createPolicy(policyPath)
+    if not policy:
+        raise RuntimeError("no policy available to doTansip()")
+        
+    if not camera:
+        raise RuntimeError("no camera passed in to doTansip()")
     
-    # dataManager for geometry information
-    #if not mgr:
-    #    mgr      = data.Manager(instrument="HSC", rerun=rerun)
-    #camera = cameraGeomUtils.makeCamera(mgr.getGeomPolicy())
-
-    print "len(match)=%d order=(%d,%d)" % (len(matchListAllCcd),
-                                           APROPPolicy.get('SIPORDER'),
-                                           APROPPolicy.get('PSIPORDER'))
+    print "len(match)=%d order=(%d,%d,%d)" % (len(matchListAllCcd),
+                                           policy.get('LSIPORDER'),
+                                           policy.get('SIPORDER'),
+                                           policy.get('PSIPORDER'))
     for i in range(len(matchListAllCcd)):
         m = matchListAllCcd[i]
         if not m:
             print "empty list for ccd %i: %s" % (i, m)
             matchListAllCcd[i] = []
             
-    resultWcs_V = hscTansip.F_WCS_TANSIP_V(matchListAllCcd, APROPPolicy, camera)
+    resultWcs_V = hscTansip.F_WCS_TANSIP_V(matchListAllCcd, policy, camera)
 
     print '... TAN-SIP fitting done.'
-    print 'size of resultWcs_V: ', len(resultWcs_V)
 
     return resultWcs_V
