045b06a35ac05145492794d9ce93760610aea4a7
Paul Price <price@astro.princeton.edu>
2013-08-02 10:00:36 -0400

    F_CALCVALUE: remove old code
    Code was replaced, no longer needed.

--- src/WCS_TANSIP_SUB.cc
+++ src/WCS_TANSIP_SUB.cc
@@ -199,20 +199,6 @@ double	F_INTEGRAL_R(int ORDER, double MAX_R, double **FUNCTION){
 	return Z;
 }
 double  F_CALCVALUE(int ORDER,double *Coef,double *POS){
-
-/*    double value = Coef[0]; // x^0 y^0
-    double x = X[0], y = X[1];
-    double xValue = 1.0;
-    for (int i = 0, ij = 0; i <= ORDER; ++i) {
-        double yValue = 1.0;
-        for (int j = 0; j <= ORDER - i; ++j, ++ij) {
-            value += Coef[ij]*xValue*yValue;
-            yValue *= y;
-        }
-        xValue *= x;
-    }
-    return value;
-*/
     int i,j,ij;
     double Z;
     double *X,*Y;
